eb43e12aa63718942c133842526fda8d09492988
William Rothermel <willrothermel@gmail.com>
2017-06-09 11:52:29 -0400

    Some styling added, first presentation-ready version

--- app/controllers/chemistry_controller.rb
+++ app/controllers/chemistry_controller.rb
@@ -6,7 +6,7 @@ class ChemistryController < ApplicationController
   def element_pair
     element1 = params[:element1]
     paired_elements = Molecule.where('atom1 = ? or atom2 = ?', element1, element1).pluck(:atom1, :atom2).flatten.uniq
-    render json: paired_elements.unshift(["Select an Element", nil])
+    render json: paired_elements.unshift(["Select an Element"])
   end
 
   def molecule_bonds
@@ -15,7 +15,7 @@ class ChemistryController < ApplicationController
     paired_elements = Molecule.where(atom1: atom1, atom2: atom2)
     paired_elements += Molecule.where(atom1: atom2, atom2: atom1)
     paired_elements = paired_elements.pluck(:bonds).uniq
-    render json: paired_elements.unshift(["Select an Bond Value", nil])
+    render json: paired_elements.unshift(["Select a Bond Value"])
   end
 
   def calculate_enthalpy
@@ -31,7 +31,9 @@ class ChemistryController < ApplicationController
         energies[type] += energy
       end
     end
-    @delta_enthalpy = energies[:product] - energies[:reactant]
+    @reactant_energy = energies[:reactant]
+    @product_energy = energies[:product]
+    @enthalpy = energies[:product] - energies[:reactant]
   end
 
 
